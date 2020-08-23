5f17d782d81c5acf3cac1b6c65ba6d5e2b531407
Magdalena1610 <66616753+Magdalena1610@users.noreply.github.com>
2020-08-23 14:15:01 +0200

    view street

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+import parkingi from './data/parkingi';
+
 import './App.css';
 
-function App() {
-  return (
-    <div >
-      Działa
-    </div>
-  );
-}
+class App extends Component {
 
+
+
+  render(){
+    return(
+      <div>
+        {parkingi.features.map(park =><div key={park.properties.street+park.properties.spots}> {park.properties.street}</div>)}
+      </div>
+    )
+  }
+}
 export default App;

29c75eb7f4fb247ce228f8a401363826c4853cdf
Magdalena1610 <66616753+Magdalena1610@users.noreply.github.com>
2020-08-28 22:21:32 +0200

    działa tablica parkingów

--- src/ParkingiRest.js
+++ src/ParkingiRest.js
@@ -1,37 +1,41 @@
 import React, { Component } from "react";
-import MaterialTable from 'material-table';
-
-
+import MaterialTable from "material-table";
 
 class ParkingiRest extends Component {
-    state = {
-        columns: [
-            { title: 'Nazwa parkingu', field: 'name' },
-            // { title: 'Ilość wolnych miejsc', field: '' },
-          ],
-          data: [],
-    }
-  
+  state = {
+    columns: [
+      { title: "Nazwa parkingu", field: "name" },
+      { title: "Ilość wolnych miejsc", field: "freePlaces" },
+    ],
+    data: [],
+  };
 
-  componentDidMount () {
+  componentDidMount() {
+    const options = { method: "GET", headers: { Origin: "*" } };
+    fetch("https://rekrutacja-sf.herokuapp.com/parking", options)
+      .then((response) => response.json())
+      .then((data) => {
+        let newData = [];
 
-    const options = { method: 'GET', headers: { Origin: '*' } };
-    fetch('https://rekrutacja-sf.herokuapp.com/parking', options)
-        .then(response =>response.json()) 
-        .then(data => {
+        data.forEach((element) => {
+          let counter = 0;
+          console.log(element.places);
 
+          for (const [key, value] of Object.entries(element.places)) {
+            if (value) {
+              counter += 1;
+            }
+            console.log(`${key}: ${value}`);
+          }
 
-
-
-            
-            this.setState({
-                data,
-            })
-            console.log(this.state.data)}
-        )
-        .catch(error => console.log('error while fetching ', error));
-
-
+          newData.push({ name: element.name, freePlaces: counter });
+        });
+        console.log(newData);
+        this.setState({
+          data: newData,
+        });
+      })
+      .catch((error) => console.log("error while fetching ", error));
   }
   render() {
     return (
@@ -40,57 +44,53 @@ class ParkingiRest extends Component {
         columns={this.state.columns}
         data={this.state.data}
         localization={{
-            body: {
-                emptyDataSourceMessage: "Brak rekordów do wyświetlenia.",
-                addTooltip: 'Dodaj',
-                deleteTooltip: 'Usuń',
-                editTooltip: 'Edytuj',
-                filterRow: {
-                    filterTooltip: 'Filtr'
-                },
-                editRow: {
-                    deleteText: 'Czy chcesz usunąć?',
-                    cancelTooltip: 'Anuluj',
-                    saveTooltip: 'Zapisz'
-                }
+          body: {
+            emptyDataSourceMessage: "Brak rekordów do wyświetlenia.",
+            addTooltip: "Dodaj",
+            deleteTooltip: "Usuń",
+            editTooltip: "Edytuj",
+            filterRow: {
+              filterTooltip: "Filtr",
             },
-            grouping: {
-                groupedBy: 'Grupuj według:'
+            editRow: {
+              deleteText: "Czy chcesz usunąć?",
+              cancelTooltip: "Anuluj",
+              saveTooltip: "Zapisz",
             },
-            header: {
-                actions: 'Akcje'
-            },
-            pagination: {
-                labelDisplayedRows: '{from}-{to} z {count}',
-                labelRowsSelect: 'Rekordów',
-                labelRowsPerPage: 'Rekordów na stronie:',
-                firstAriaLabel: 'Pierwsza strona',
-                firstTooltip: 'Pierwsza strona',
-                previousAriaLabel: 'Poprzednia strona',
-                previousTooltip: 'Poprzednia strona',
-                nextAriaLabel: 'Następna strona',
-                nextTooltip: 'Następna strona',
-                lastAriaLabel: 'Ostatnia strona',
-                lastTooltip: 'Ostatnia strona'
-            },
-            toolbar: {
-                nRowsSelected: '{0} rekordów(s) wybranych(s)',
-                showColumnsTitle: 'Wyświetl kolumny',
-                showColumnsAriaLabel: 'Wyświetl kolumny',
-                exportTitle: 'Eksportuj',
-                exportAriaLabel: 'Eksportuj',
-                exportName: 'Eksportuj jako CSV',
-                searchTooltip: 'Szukaj',
-                searchPlaceholder: 'Szukaj'
-            }
-       }}
-        />
-    )
+          },
+          grouping: {
+            groupedBy: "Grupuj według:",
+          },
+          header: {
+            actions: "Akcje",
+          },
+          pagination: {
+            labelDisplayedRows: "{from}-{to} z {count}",
+            labelRowsSelect: "Rekordów",
+            labelRowsPerPage: "Rekordów na stronie:",
+            firstAriaLabel: "Pierwsza strona",
+            firstTooltip: "Pierwsza strona",
+            previousAriaLabel: "Poprzednia strona",
+            previousTooltip: "Poprzednia strona",
+            nextAriaLabel: "Następna strona",
+            nextTooltip: "Następna strona",
+            lastAriaLabel: "Ostatnia strona",
+            lastTooltip: "Ostatnia strona",
+          },
+          toolbar: {
+            nRowsSelected: "{0} rekordów(s) wybranych(s)",
+            showColumnsTitle: "Wyświetl kolumny",
+            showColumnsAriaLabel: "Wyświetl kolumny",
+            exportTitle: "Eksportuj",
+            exportAriaLabel: "Eksportuj",
+            exportName: "Eksportuj jako CSV",
+            searchTooltip: "Szukaj",
+            searchPlaceholder: "Szukaj",
+          },
+        }}
+      />
+    );
   }
 }
 
 export default ParkingiRest;
-
-
-
-        

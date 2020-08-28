b434c82cf8db2ab83173f99b6eaf8e53a378d1e2
Magdalena1610 <66616753+Magdalena1610@users.noreply.github.com>
2020-08-28 18:23:57 +0200

    Popup działa

--- src/MapView.js
+++ src/MapView.js
@@ -6,13 +6,19 @@ import Feature from 'ol/Feature';
 import GeoJSON from 'ol/format/GeoJSON';
 import Map from 'ol/Map';
 import View from 'ol/View';
+import Select from 'ol/interaction/Select';
+import Overlay from 'ol/Overlay';
 import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
 import {Vector as VectorSource} from 'ol/source';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
-import WKT from 'ol/format/WKT';
 import {fromLonLat} from 'ol/proj';
+import {toLonLat} from 'ol/proj';
 import {useGeographic} from 'ol/proj';
+import {toStringHDMS} from 'ol/coordinate';
+import {altKeyOnly, click, pointerMove} from 'ol/events/condition';
+
 import parkingi from './data/parkingi';
+import './MapView.css';
 import App from './App';
 
 let styles = {
@@ -31,19 +37,32 @@ let styles = {
 let styleFunction = function (feature) {
   return styles[feature.getGeometry().getType()];
 };
+
+
+
 class MapView extends Component {
 
   state = {
       center: [18.606363236904144,54.38519346546863],
       zoom: 16,
       dataParkingi : parkingi,
-      vectorLayer :null,
-      olmap: null,
-    };
-  
-    
-  
+    }
+
+   
+
   componentDidMount() {
+    let container = this.refs.popup;
+    let content = this.refs.popupContent;
+
+    let overlay = new Overlay({
+      element: container,
+      autoPan: true,
+      autoPanAnimation: {
+        duration: 250,
+      },
+    });
+
+    
     let vectorSource = new VectorSource({
       features: new GeoJSON().readFeatures(this.state.dataParkingi),
     });
@@ -51,8 +70,12 @@ class MapView extends Component {
        source: vectorSource,
      style: styleFunction
    });
-   
-    this.state.olmap = new Map({
+
+   let select = new Select({
+    condition: pointerMove,
+  });
+  
+    let olmap = new Map({
        target: this.refs.mapContainer,
        layers: [
          new TileLayer({
@@ -60,38 +83,44 @@ class MapView extends Component {
          }),
          vectorLayer
        ],
+       overlays: [overlay],
        view: new View({
          center:fromLonLat(this.state.center),
          zoom: this.state.zoom
        })
      });
-    console.log('componentDidMount')
-     useGeographic(); 
-    //  this.olmap.setTarget("map");   
+     olmap.addInteraction(select);
+     select.on('select', function (e) {
+       
+      if ( e.selected.length >0){
+        let coordinate = e.selected[0].getGeometry().getCoordinates()
+        let properties = e.selected[0].getProperties();
+        console.log(properties.street);
+        console.log(coordinate[0][0]);
+        let ifPaid ='';
+        if(properties.paid){
+          ifPaid='Tak';
+        }else{
+          ifPaid ='Nie';
+        }
+      
+        content.innerHTML = '<span>Ulica: <b>'+ properties.street +'</b></span><br>'+
+                            '<span>Liczba miejsc parkingowych: <b>'+ properties.spots +'</b></span><br>'+
+                            '<span>W tym dla niepełnosprawnych: <b>'+ properties.handicappedSpots +'</b></span><br>'+
+                            '<span>Płatny: <b>'+ ifPaid +'</b></span><br><br>'+
+                            'Lokalizacja: <code>' + coordinate[0][0] + '</code>';
+        overlay.setPosition(coordinate[0][0]);
+
+     
+
+    }
+     });
+
+   
+    useGeographic(); 
   };
 
-  // vectorSource = new VectorSource({
-  //     features: new GeoJSON().readFeatures(this.state.dataParkingi),
-  //   });
-  // vectorLayer = new VectorLayer({
-  //     source: this.vectorSource,
-  //     style: styleFunction
-  // });
-
-  // olmap = new Map({
-  //     target: null,
-  //     layers: [
-  //       new TileLayer({
-  //         source: new OSM()
-  //       }),
-  //       this.vectorLayer
-  //     ],
-  //     view: new View({
-  //       center:fromLonLat(this.state.center),
-  //       zoom: this.state.zoom
-  //     })
-  //   });
-  // ;
+  
 
   handleDeleteRow = (oldData) =>{
     const dataPF = [...this.state.dataParkingi.features]
@@ -107,62 +136,13 @@ class MapView extends Component {
     // this.vectorSource.refresh()
     // this.olmap.render() 
   };
-  // componentDidUpdate(prevState) {
-  //   // Typowy sposób użycia (nie zapomnij porównać właściwości):
-  //   if (prevState.dataParkingi !== this.state.dataParkingi) {
-  //      let feature = new Feature(new GeoJSON().readFeatures(prevState.dataParkingi));
-  //      this.vectorSource.addFeature(feature);
-  //      this.olmap.addLayer(this.vectorLayer)
-  //    console.log(prevState.dataParkingi);
-  //   }
+ 
 
   componentDidUpdate(prevProps, prevState) {
     console.log('Prev state', prevState.dataParkingi); // Before update
     console.log('New state', this.state.dataParkingi); // After update 
-
-    if(prevState.dataParkingi !== this.state.dataParkingi){
-      let vectorSource = new VectorSource({
-        features: new GeoJSON().readFeatures(this.state.dataParkingi),
-      });
-      let vectorLayer = new VectorLayer({
-         source: vectorSource,
-       style: styleFunction
-     });
-       let olmap = new Map({
-          target: this.refs.mapUpdate,
-          layers: [
-            new TileLayer({
-              source: new OSM()
-            }),
-            vectorLayer
-          ],
-          view: new View({
-            center:fromLonLat(this.state.center),
-            zoom: this.state.zoom,
-          }),     
-      })
-    //   this.setState({
-    //     olmap,
-    //   })
-      useGeographic(); 
-     //  this.olmap.setTarget("map");   
-     
-    }
   }
-    
-    // this.state.vectorSource.addFeature(new Feature(new Circle([5e6, 7e6], 1e6)));
-    // let feature = new Feature(this.state.dataParkingi.features);
-    // this.vectorSource.addFeature(feature);
-    // console.log(this.vectorSource.features)
-    // this.vectorLayer.setSource({
-    //     features: this.state.dataParkingi
-    //   });
-    // this.state.featuresLayer.setSource(
-    //   new VectorSource({
-    //     features: this.state.dataParkingi
-    //   })
-    // ); 
-  
+
   handleUpdateRow = (oldata, newData) =>{    
     this.setState((previousState)=> {
       const data = [...previousState.dataParkingi.features];
@@ -177,7 +157,9 @@ class MapView extends Component {
       <div>
         <App dataParkingi={this.state.dataParkingi.features} functionUpdate={this.handleUpdateRow} functionDelete={this.handleDeleteRow}/>
         <div ref="mapContainer" style={{ width: "100%", height: "500px" }}></div>
-        <div ref="mapUpdate" style={{ width: "100%", height: "500px" }}></div>
+        <div ref="popup" class="ol-popup">    
+          <div ref="popupContent"></div>
+        </div>
       </div>
       
     );
